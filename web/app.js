0f16d7b9198bdd270a108913ded4b8445fd94e8e
André Nogueira <andre.nogueira@epfl.ch>
2018-11-14 11:02:05 +0100

    fix: update HOSTED_VIEWER_ORIGINS w graasp-api subdomains
    close #6

--- web/app.js
+++ web/app.js
@@ -1758,6 +1758,18 @@ if (typeof PDFJSDev === "undefined" || PDFJSDev.test("GENERIC")) {
     "https://viewer.graasp.eu",
     "http://viewer.dev.graasp.eu",
     "https://viewer.dev.graasp.eu",
+    "http://graasp-users.api.dev.graasp.eu",
+    "https://graasp-users.api.dev.graasp.eu",
+    "http://light-users.api.dev.graasp.eu",
+    "https://light-users.api.dev.graasp.eu",
+    "http://api.dev.graasp.eu",
+    "https://api.dev.graasp.eu",
+    "http://graasp-users.api.graasp.eu",
+    "https://graasp-users.api.graasp.eu",
+    "http://light-users.api.graasp.eu",
+    "https://light-users.api.graasp.eu",
+    "http://api.graasp.eu",
+    "https://api.graasp.eu",
   ];
   validateFileURL = function(file) {
     if (file === undefined) {
